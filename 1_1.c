d29f1a110085b42a4819a7359babfd88f958cf6c
BondGottor <108526121+BondGottor@users.noreply.github.com>
2023-03-18 18:59:45 +0300

    Update 1_1.c

--- 1_1.c
+++ 1_1.c
@@ -1,80 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int argc, char **argv) {
-  if (argc == 1) {
-    printf("No arguments\n");
-    return 0;
+void Multiplicity(int numm){
+  if (numm>100){
+    printf("нет кратных чисел в пределе до 100\n");
+    return;
   }
-  if (argc > 3) {
-    printf("Too many arguments\n");
-    return 0;
+  for (int i=numm;i<=100;i+=numm){
+    printf("%d \n", i);
+    }
   }
-  int num = 0;
+int Sqrt(int num){
+  int right=num;
+  int left=0;
+  int mid=0;
+  for (int i=num/2;i!=0;i/=2){
+    mid=right-i;
+    if (mid*mid>num){
+      right=mid;
+    }
+    else if (mid*mid<num){
+      left=mid;
+    }
+    else{
+      return mid;
+    }
+  }
+}
+void Prime(int nump){
+  if (nump==0 || nump==1){
+    printf("не простое не сложное");
+    return;
+  }
+  int max=Sqrt(nump)+1;
+  for(int i=2;i<=max;i+=1){
+   if(nump%i==0){
+     printf("составное\n");
+     return;
+   }
+    }
+    printf("простое\n");
+}
+void ChByCh(int numc){
+  int ten=10;
+    while(numc/ten){
+    ten*=10;
+  }
+  while(numc){
+    ten/=10;
+    printf("%d ",numc/ten);
+    numc%=ten;
+  }
+  printf("\n");
+}
+void Exponentiation(int nume){
+  if (nume>10){
+    printf("должно быть не больше 10\n");
+    return;
+  }
+  int prnt=1;
+  for (int i=1;i<=10;i++){
+    prnt=i;
+    for (int j=1;j<=nume;j++){
+      printf("%d ",prnt);
+      prnt*=i;
+    }
+    printf("\n");
+    }
+  }
+void Summa(int nums){
+  long unsigned int prnt=(nums * (nums + 1)) / 2;
+  printf("%lu\n", prnt);
+  }
+void Factorial(int numf){
+  long long unsigned int fact=1;
+  for (int i=2;i<=numf;i++){
+    fact*=i;
+    }
+  printf("%llu \n",fact);
+}
+int main(int argc, char** argv){
   int i=0;
-  if (argv[2][0] == '-') {
-    printf("Second argument must be a positive number\n");
-    return 0;
-  }
+  int num=0;
   while(argv[2][i]){
     num = num * 10 + (argv[2][i] - '0');
     i+=1;}
-  if (!strcmp(argv[1], "-h")) {
-    if (num > 100) {
-      printf("No multiples\n");
-      return 0;
+  printf("%d \n", argv[1][1]);
+  switch (argv[1][1]){
+    case 'h': Multiplicity(num);
+    case 'p': Prime(num);
+    case 's': ChByCh(num);
+    case 'e': Exponentiation(num);
+    case 'a': Summa(num);
+    case 'f': Factorial(num);
     }
-    for (int i = num; i <= 100; i += num) {
-      printf("%d ", i);
-      printf("\n");
-    }
-  } 
-  else if (!strcmp(argv[1], "-p")) {
-    if (num <= 1)
-      printf("Number is neither simple nor composite\n");
-    else {
-      for (unsigned int i = 2; i < num; i++)
-        if (num % i == 0) {
-          printf("Number is composite\n");
-          return 0;
-        }
-      printf("Number is simple\n");
-    }
-  }
-  else if (!strcmp(argv[1], "-s")) {
-    for (int i = 0; i <= strlen(argv[2]); i++) {
-      printf("%c ", argv[2][i]);
-      printf(" ");
-    }
-    printf("\n");
-  }
-  else if (!strcmp(argv[1], "-e")) {
-    if (num > 10)
-      printf("Incorrect number for this argument\n");
-    else {
-      for (int i = 1; i <= 10; i++) {
-        int p = i;
-        for (int j = 1; j <= num; j++) {
-          printf("%d ", p);
-          p *= i;
-        }
-        printf("\n");
-      }
-    }
-  } 
-  else if (!strcmp(argv[1], "-a")) {
-    printf("%d\n", (num * (num + 1)) / 2);
-  } 
-  else if (!strcmp(argv[1], "-f")) {
-    unsigned int res = 1;
-    for (int i = 2; i <= num; i++) {
-      res *= i;
-    }
-    printf("%d\n", res);
-  } 
-  else {
-    printf("Wrong argument(s)\n");
-    return 0;
-  }
-  return 0;
 }
